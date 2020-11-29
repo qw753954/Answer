f982df39467cbe6a4f66bb90fc8806132ae72478
qw753954 <68770475+qw753954@users.noreply.github.com>
2020-11-29 20:05:52 +0800

    Update 2020-11-29T12:05:43.186Z

---
+++ assets/js/all.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(function () {
+  // $("p").text("The DOM is now loaded and can be manipulated.");
+  $("#reply").click(function () {
+    $(".reply").addClass('active');
+  });
+  $('#replyCancel').click(function () {
+    $(".reply").removeClass('active');
+  });
+  $('.fontEdit').click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+  });
+  $('#expand').click(function (event) {
+    event.preventDefault();
+    $(this).find('i').toggleClass('active');
+    $('.expand').slideToggle();
+  });
+});
+//# sourceMappingURL=all.js.map
